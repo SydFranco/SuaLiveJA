4fff8e87b534b1b48919b37c004606be596fa414
Anderson Cunha <cunhaanderson60@gmail.com>
2023-01-24 13:27:16 -0300

    Feito Falta ajeitar o Update data Base

--- SuaLiveJA/Data/ApplicationDbContext.cs
+++ SuaLiveJA/Data/ApplicationDbContext.cs
@@ -9,5 +9,10 @@ namespace SuaLiveJA.Data
             : base(options)
         {
         }
+        public DbSet<SuaLiveJA.Models.Contato> Cotato { get; set; } = default!;
+        public DbSet<SuaLiveJA.Models.Evento> Evento { get; set; }
+        public DbSet<SuaLiveJA.Models.Iasd> Iasd { get; set; }
+        public DbSet<SuaLiveJA.Models.Secao> Secao { get; set; }
+        public DbSet<SuaLiveJA.Models.Usuario> Usuario { get; set; }
     }
 }
